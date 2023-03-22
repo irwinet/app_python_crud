bf91bbe701dc06759e6f5abf7ef02b76543d9e2c
Irwin Estrada <ext_iestrada@tottus.com.pe>
2023-03-22 15:25:56 -0500

    Add list at crud clients

---
+++ main.py
@@ -0,0 +1,103 @@
+import sys
+
+clients = ['irwin','pablo']
+
+def create_client(client_name):
+    global clients
+    if client_name not in clients:
+        clients.append(client_name)
+    else:
+        print('Client already is in the client\'s list')
+
+def list_clients():
+    global clients
+    for idx, client in enumerate(clients):
+        print('{}: {}'.format(idx, client))
+
+def update_client(client_name, update_client_name):
+    global clients
+    if client_name in clients:
+        index = clients.index(client_name)
+        clients[index] = update_client_name
+    else:
+        print('Client is not in clients list')
+
+def delete_client(client_name):
+    global clients
+    if client_name in clients:
+        clients.remove(client_name)
+    else:
+        print('Client is not in clients list')
+
+def search_client(client_name):
+    global clients
+    for client in clients:
+        if client != client_name:
+            continue
+        else:
+            return True
+
+def fibonacci(max):
+    a, b = 0, 1
+    while a < max:
+        yield a
+        a, b = b, a+b            
+
+def _add_comma():
+    global clients
+    clients+=','
+
+def _print_welcome():
+    print("WELCOME TO PLATZI VENTAS")
+    print('*'*50)
+    print('What would you like to do today?')
+    print('[C]reate client')
+    print('[U]pdate client')
+    print('[D]elete client')
+    print('[S]earch client')
+    print('[F]ibonacci')
+
+def _get_client_name():
+    client_name = None
+    
+    while not client_name:
+        client_name = input('What si the client name? ')
+        if client_name == 'exit':
+            client_name = None
+            break
+    
+    if not client_name:
+        sys.exit()
+
+    return client_name
+
+if __name__ == '__main__':
+    _print_welcome()
+    command = input()
+    command = command.upper()
+    if command == 'C':
+        client_name = _get_client_name()
+        create_client(client_name)
+        list_clients()
+    elif command == 'D':
+        client_name = _get_client_name()
+        delete_client(client_name)
+        list_clients()
+    elif command == 'U':
+        client_name = _get_client_name()
+        update_client_name = input('What is the updated client name? ')
+        update_client(client_name, update_client_name)
+        list_clients()
+    elif command == 'S':
+        client_name = _get_client_name()
+        found = search_client(client_name)
+        if found:
+            print('The client is in the client\'s list')
+        else:
+            print('The client: {} is not in our client\'s list'.format(client_name))
+    elif command == 'F':
+        fib1 = fibonacci(10)
+        fib_nums = [num for num in fib1]
+        print(fib_nums)
+    else:
+        print('Invalid command')
